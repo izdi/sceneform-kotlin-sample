8132426acbbea6af351d3119dcbc508407159272
Sergey Keller <izdieu@gmail.com>
2018-07-04 23:12:26 +0300

    ┬─┬﻿ ノ( ゜-゜ノ)

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Sceneform experiments in Kotlin
 
-* Placing objects on a Point
+* Placing objects on a `Point`
+* Renderable feature `Point` for debugging purpose
+* Custom object rotation independed on `AnchoreNode`
--- app/src/main/java/pro/izdi/sceneformkt/MainActivity.kt
+++ app/src/main/java/pro/izdi/sceneformkt/MainActivity.kt
@@ -7,13 +7,18 @@ import android.view.GestureDetector
 import android.view.Gravity
 import android.view.MotionEvent
 import android.widget.Toast
+
 import com.google.ar.core.Point
 import com.google.ar.core.TrackingState
 import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.FrameTime
+import com.google.ar.sceneform.Node
+import com.google.ar.sceneform.math.Quaternion
 import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.ux.ArFragment
 import com.google.ar.sceneform.ux.TransformableNode
 
+import pro.izdi.sceneformkt.nodes.PointCloudNode
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,6 +29,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var andyRenderable: ModelRenderable
     lateinit var arFragment: ArFragment
 
+    lateinit var pointCloudNode: PointCloudNode
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -32,6 +39,13 @@ class MainActivity : AppCompatActivity() {
         arFragment = supportFragmentManager.findFragmentById(R.id.ux_fragment) as ArFragment
         arFragment.planeDiscoveryController.hide()
         arFragment.planeDiscoveryController.setInstructionView(null)
+
+        pointCloudNode = PointCloudNode(this)
+        arFragment.arSceneView.scene.addChild(pointCloudNode)
+
+        arFragment.arSceneView.planeRenderer.isEnabled = false
+
+        arFragment.arSceneView.scene.setOnUpdateListener(::onFrame)
 
         ModelRenderable.builder()
             .setSource(this, R.raw.andy)
@@ -68,11 +82,31 @@ class MainActivity : AppCompatActivity() {
         )
     }
 
+    fun onFrame(frameTime: FrameTime) {
+        arFragment.onUpdate(frameTime)
+        val frame = arFragment.arSceneView.arFrame
+        val camera = frame.camera
+
+        // If not tracking, don't draw 3d objects.
+        if (camera.trackingState == TrackingState.PAUSED) {
+            return
+        }
+
+        // Visualize tracked points.
+        val pointCloud = frame.acquirePointCloud()
+        pointCloudNode.update(pointCloud)
+
+        // Application is responsible for releasing the point cloud resources after using it.
+        pointCloud.release()
+
+    }
+
     fun onSingleTap(motionEvent: MotionEvent) {
         val arSceneView = arFragment.arSceneView
         val frame = arSceneView.arFrame
         if (frame != null && frame.camera.trackingState == TrackingState.TRACKING) {
             for (hit in frame.hitTest(motionEvent)) {
+                Log.i(TAG, "Distance to a hit: ${hit.distance}")
                 val trackable = hit.trackable
                 if (trackable is Point) {
                     // Anchor down
@@ -80,9 +114,13 @@ class MainActivity : AppCompatActivity() {
                     val anchorNode = AnchorNode(anchor)
                     anchorNode.setParent(arSceneView.scene)
 
+                    val node = Node()
+                    node.setParent(anchorNode)
+                    node.worldRotation = Quaternion()
+
                     // Create node and add to the anchor
                     val andy = TransformableNode(arFragment.transformationSystem)
-                    andy.setParent(anchorNode)
+                    andy.setParent(node)
                     andy.renderable = andyRenderable
                     andy.select()
                 }
